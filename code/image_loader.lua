49369fc70f9816fa8a1107753aa79e98ccae9663
MattJTrueblood <mattjtrueblood@gmail.com>
2024-07-19 04:44:13 -0700

    fixing other tab/space crap

--- code/image_loader.lua
+++ code/image_loader.lua
@@ -3,21 +3,21 @@ local constants = require("constants")
 local mymodule = {}
 
 function mymodule.loadImageTiles(file, tileSize)
-	local tilesetImage = love.graphics.newImage(file)
-	local tileWidth, tileHeight = tileSize, tileSize
-	local tilesetWidth, tilesetHeight = tilesetImage:getWidth(), tilesetImage:getHeight()
-	local numCols, numRows = tilesetWidth / tileWidth, tilesetHeight / tileHeight
-	local tileMap = {}
+    local tilesetImage = love.graphics.newImage(file)
+    local tileWidth, tileHeight = tileSize, tileSize
+    local tilesetWidth, tilesetHeight = tilesetImage:getWidth(), tilesetImage:getHeight()
+    local numCols, numRows = tilesetWidth / tileWidth, tilesetHeight / tileHeight
+    local tileMap = {}
 
-	for i=1, numRows do
-		for j=1, numCols do
-			tileMap[(((i-1) * numCols) + j)] = 
-				love.graphics.newQuad((j-1) * tileWidth,
-			 	(i-1) * tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
-		end
-	end
+    for i=1, numRows do
+        for j=1, numCols do
+            tileMap[(((i-1) * numCols) + j)] = 
+                love.graphics.newQuad((j-1) * tileWidth,
+                (i-1) * tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
+        end
+    end
 
-	return tilesetImage, tileMap
+    return tilesetImage, tileMap
 end
 
 -- first, load all images used by all sprite collections
